4ec29b5cfdd93b02a582d835e8d9c14f3358cd82
Damon Cook <damonacook@gmail.com>
2019-02-08 17:21:19 -0500

    let’s kick things off 💥

---
+++ block-example.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Block Examples
+ * Description: This is a plugin demonstrating how to build blocks for the Gutenberg editor.
+ * Text Domain: block-examples
+ * Version: 0.1.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+include '01-basic-block/index.php'
